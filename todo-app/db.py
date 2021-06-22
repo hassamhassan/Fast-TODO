e4d8195394cfd33c2c5e06da85ae5c4bb99c335d
hassamhassan <hassamhassan.qc@gmail.com>
2021-06-22 20:46:19 +0500

    connection string updated

--- todo-app/db.py
+++ todo-app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:lzt25@127.0.0.1:5432"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:lzt25@127.0.0.1:5432/postgres"
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:lzt25@db:5432/postgres"
 
 
